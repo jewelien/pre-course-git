97d50d23ed71fea923e269a184336b2d988ce9d6
Julien Guanzon <julienguanzon@gmail.com>
2015-01-21 01:51:43 -0700

    Adds BMI Calc

--- pre-course-git/AppDelegate.m
+++ pre-course-git/AppDelegate.m
@@ -16,6 +16,20 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //Declare two variables
+    CGFloat height;
+    CGFloat weight;
+    
+    //Store a value in each variable
+    height = 71.0f;
+    weight = 185.0f;
+    
+    //We'll calculate the body mass index
+    CGFloat bodyMassIndex = (weight / (height * height)) * 703;
+    
+    //log to the console
+    NSLog(@"%f", bodyMassIndex);
+    
     // Override point for customization after application launch.
     return YES;
 }
